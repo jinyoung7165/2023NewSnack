a531b96c581e1beb378876eb8369161ced99840f
jooda00 <dragon2000a@naver.com>
2023-01-21 20:54:09 +0900

    [FEAT] 하루씩 증가하며 whole_world추가 로직 & 성능(Skip-gram)

--- custom_word2vec.py
+++ custom_word2vec.py
@@ -3,7 +3,6 @@ from s3_method import S3
 import datetime
 from gensim.models import Word2Vec
 
-now_date = datetime.datetime.now().date()
 class customWord2Vec:
     def __init__(self, docToText: DocToText):
         self.docToText = docToText
@@ -15,14 +14,42 @@ class customWord2Vec:
         date 특정 날짜까지 계속 증가하게 하는 코드 만들기(start, end 날짜 알 때)
         병렬 처리
         '''
-        self.docToText.csv_to_text(date, "sbs.csv")
-        self.docs = list(self.docToText.main) # ["첫번째 문서 두번째 문장", "두번째 문서 두번째 문장",]
         whole_word = []
-        for doc in self.docs:
-            after_stopword = self.docToText.sentence_tokenizer(doc)
-            if after_stopword: whole_word.append(after_stopword)
-        self.model = Word2Vec(sentences = whole_word, min_count = 1, window = 5, workers = 4, sg = 0)
+
+        # 날짜들을 전역으로 하면 안되는 이유는..?
+        end_date = datetime.datetime.now() # end_date는 오늘!
+        now_date = end_date - datetime.timedelta(days=1) # 일단 now_date는 어제 기준..
+        delta = datetime.timedelta(days=1) # 1일 후
         
+        while True:
+            # print(now_date.date())
+            self.docToText.csv_to_text("{}".format(now_date.date()), "sbs.csv")
+            self.docs = list(self.docToText.main) # ["첫번째 문서 두번째 문장", "두번째 문서 두번째 문장",]
+            for doc in self.docs:
+                after_stopword = self.docToText.sentence_tokenizer(doc)
+                if after_stopword: whole_word.append(after_stopword)
+            
+            # skip-gram이 좋은 것 같다.'외교부'는 '이란'이 가장 동일하게 나옴
+            self.model = Word2Vec(sentences = whole_word, min_count = 1, window = 5, workers = 4, sg = 1) 
+            
+            # 만약 end_date에 다다르면 while문 종료
+            if(now_date.date() == end_date.date()):
+                break
+            now_date += delta # 1일씩 증가해주기
+            
+
+        # for train_date in (now.date(), end_date):
+        #     self.docToText.csv_to_text(train_date, "sbs.csv")
+        #     self.docs = list(self.docToText.main) # ["첫번째 문서 두번째 문장", "두번째 문서 두번째 문장",]
+        #     whole_word = []
+        #     for doc in self.docs:
+        #         after_stopword = self.docToText.sentence_tokenizer(doc)
+        #         if after_stopword: whole_word.append(after_stopword)
+        #     self.model = Word2Vec(sentences = whole_word, min_count = 1, window = 5, workers = 4, sg = 1) # skip-gram이 좋은 것 같다.'외교부'는 '이란'이 가장 동일하게 나옴
+        #     train_date = train_date + datetime.timedelta(days=1)
+
+        print("whole_world's len : ", len(whole_word))
+        # print(whole_word[0][0], self.model.wv.most_similar(whole_word[0][0]))
     # def word2vec(self): 
     #     # size = 워드 벡터의 특징 값. 즉, 임베딩 된 벡터의 차원.
     #     # window = 컨텍스트 윈도우 크기
