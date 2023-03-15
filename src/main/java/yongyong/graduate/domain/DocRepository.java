970bd90b0e70458f4bfc0f10e1bb0fb32a3cc400
jooda00 <dragon2000a@naver.com>
2023-03-15 20:28:10 +0900

    [FIX] DocRepository delete

--- src/main/java/yongyong/graduate/domain/DocRepository.java
+++
@@ -1,8 +0,0 @@
-package yongyong.graduate.domain;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DocRepository extends MongoRepository<Doc, String> {
-}
