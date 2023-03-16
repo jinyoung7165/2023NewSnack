65dece01ba1957a04dc4e45ea4457ebee30a4206
young <jinyoung7165@gmail.com>
2023-03-17 08:27:29 +0900

    [FEAT] S3에서 mongoDB로 전환 & 쿼리 조건으로 _idx 사용

--- run.py
+++ run.py
@@ -1,26 +1,24 @@
-import datetime, time
+import datetime, time, collections, sys, os, requests
 from dotenv import load_dotenv
-import collections
 from gensim.models import Word2Vec
+from pymongo import MongoClient
 
-import sys, os, requests
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from remote.s3_method import S3
-from preprocess.doc_text import DocToText
 from preprocess.tokenizer import Tokenizer
 from weighting.doc_tfidf import DocTfidf
 from weighting.sentence import Sentence
-from remote.mongo_method import RunDB
+from remote.mongo_method import MongoDB, RunDB
 from summary.summary import Summary
 # load .env
 load_dotenv()
 
 def main():
-    s3 = S3() #s3 connection 1번
-
-    docToText = DocToText(s3)
-    tokenizer = Tokenizer()
+    client = MongoClient("mongodb+srv://{}:{}@yongyong.834oknp.mongodb.net"
+                .format(os.environ.get('mongo_username'), os.environ.get('mongo_password')))
+    db = client['newsnack'] #db 접근
+    mongodb = MongoDB(db)
     
+    tokenizer = Tokenizer()
     word2vec = Word2Vec.load('model_bulk')
     
     doc_word_dict = collections.defaultdict(list)
@@ -32,9 +30,9 @@ def main():
     for _ in range(3):
         today_name = today.strftime("%Y-%m-%d")
         now_t = time.time()
-        sentence = Sentence(docToText, tokenizer, word2vec, today_name, "naver_news.csv")
+        sentence = Sentence(mongodb, tokenizer, word2vec, today_name)
         sentence.doc_process()
-        doc_word_dict.update(sentence.docs_word_arr)
+        doc_word_dict.update(sentence.docs_word_arr) #date/_idx : ['단어1','단어2']
         print(time.time()- now_t) # 하루 뉴스 490개에 210-299초
         today += delta # 하루씩 증가
 
@@ -47,15 +45,14 @@ def main():
     print("hottopic", time.time()-now_t) #4초
     # DocTfidf class 3일치 -> 18분
     now_t = time.time()
-    run_db = RunDB(join_vector, hot_topic)
+    run_db = RunDB(mongodb, join_vector, hot_topic)
     run_db.setting()
     print("rundb", time.time()-now_t) #1일치 47초
 
-    doc_main_dict = run_db.doc_dict # summary에 넘겨줄 요약 대상 {"2023-02-01/0" : "본문 내용"}
-    db = run_db.db # summary에 주입할 날짜_doc collection
+    doc_main_dict = run_db.doc_dict # summary에 넘겨줄 요약 대상 {"2023-02-01/_id" : "본문 내용"}
 
     now_t = time.time()
-    summary = Summary(hot_topic, doc_main_dict, db)
+    summary = Summary(db, hot_topic, doc_main_dict)
     summary.setting()
     print("summary", time.time()-now_t) #하루치 249
     
