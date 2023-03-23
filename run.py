1eb49564381a1490c5bbe449fa7535a53700f7fa
young <jinyoung7165@gmail.com>
2023-03-23 18:51:55 +0900

    [ADD] cache sentence - dockerfile

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import datetime, time, collections, sys, os, requests
+import datetime, time, collections, sys, os, requests, pickle
 from dotenv import load_dotenv
 from gensim.models import FastText
 from pymongo import MongoClient
@@ -26,13 +26,24 @@ def main():
     delta = datetime.timedelta(days=1) # 1일 후
     end_date = datetime.datetime.now()
     today = end_date - datetime.timedelta(days=2) #이틀 전부터 실행
-    
     for _ in range(3):
         today_name = today.strftime("%Y-%m-%d")
+        file_path = '/tmp/{}.pickle'.format(today_name)
         now_t = time.time()
-        sentence = Sentence(mongodb, tokenizer, fastvec, today_name)
-        sentence.doc_process()
-        doc_word_dict.update(sentence.docs_word_arr) #date/_idx : ['단어1','단어2']
+        today_word_arr = {}
+        if os.path.exists(file_path): # 이미 해당 날짜의 sentence 처리 결과 있으면 파일 불러옴
+            print("exists")
+            with open(file_path, 'rb') as fr:
+                today_word_arr = pickle.load(fr)
+        else:
+            print("not exists")
+            sentence = Sentence(mongodb, tokenizer, fastvec, today_name) # 새로운 날짜 처리
+            sentence.doc_process()
+            with open(file_path, 'wb') as fw:
+                today_word_arr = sentence.docs_word_arr
+                pickle.dump(today_word_arr, fw)
+        
+        doc_word_dict.update(today_word_arr) #date/_idx : ['단어1','단어2']
         print(time.time()- now_t) # 하루 뉴스 490개에 210-299초
         today += delta # 하루씩 증가
 
@@ -47,14 +58,14 @@ def main():
     now_t = time.time()
     run_db = RunDB(mongodb, join_vector, hot_topic)
     run_db.setting()
-    print("rundb", time.time()-now_t) #1일치 47초
+    print("rundb", time.time()-now_t)
 
     doc_main_dict = run_db.doc_dict # summary에 넘겨줄 요약 대상 {"2023-02-01/_id" : "본문 내용"}
 
     now_t = time.time()
     summary = Summary(db, hot_topic, doc_main_dict)
     summary.setting()
-    print("summary", time.time()-now_t) #하루치 249
+    print("summary", time.time()-now_t)
     
     response = requests.get(os.environ.get('newsnack_update_uri'))
     print("today weight finished", response.text)
--- test/t.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-print("run~~~~~~~~~~~~~~~~~~")
-try:
-    print(requests.get("http://newsnack-spring.Newsnack:8080"))
-except Exception as e:
-    print("hey", e)
